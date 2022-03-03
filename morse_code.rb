29151aa946735e287c031a5c74e9e370810ecd94
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-03-03 16:01:17 +0600

    Fix linter errors

--- morse_code.rb
+++ morse_code.rb
@@ -1,60 +1,52 @@
-MORSE = { '.-' => 'A',
-'-...' => 'B',
-'-.-.' => 'C',
-'-..' => 'D',
-'.' => 'E',
-'..-.' => 'F',
-'--.' => 'G',
-'....' => 'H',
-'..' => 'I',
-'.---' => 'J',
-'-.-' => 'K',
-'.-..' => 'L',
-'--' => 'M',
-'-.' => 'N',
-'---' => 'O',
-'.--.' => 'P',
-'--.-' => 'Q',
-'.-.' => 'R',
-'...' => 'S',
-'-' => 'T',
-'..-' => 'U',
-'...-' => 'V',
-'.--' => 'W',
-'-..-' => 'X',
-'-.--' => 'Y',
-'--..' => 'Z', 
-'.----' => '1',
-'..---' => '2',
-'...--' => '3',
-'....-' => '4',
-'.....' => '5',
-'-....' => '6',
-'--...' => '7',
-'---..' => '8',
-'----.' => '9',
-'-----' => '0',
-}
+MORSE = {
+  '.-' => 'A', '-...' => 'B',
+  '-.-.' => 'C', '-..' => 'D',
+  '.' => 'E', '..-.' => 'F',
+  '--.' => 'G', '....' => 'H',
+  '..' => 'I', '.---' => 'J',
+  '-.-' => 'K', '.-..' => 'L',
+  '--' => 'M', '-.' => 'N',
+  '---' => 'O', '.--.' => 'P',
+  '--.-' => 'Q', '.-.' => 'R',
+  '...' => 'S',
+  '-' => 'T',
+  '..-' => 'U',
+  '...-' => 'V',
+  '.--' => 'W',
+  '-..-' => 'X',
+  '-.--' => 'Y',
+  '--..' => 'Z',
+  '.----' => '1',
+  '..---' => '2',
+  '...--' => '3',
+  '....-' => '4',
+  '.....' => '5',
+  '-....' => '6',
+  '--...' => '7',
+  '---..' => '8',
+  '----.' => '9',
+  '-----' => '0'
+}.freeze
 
 def decode_char(code)
-  return MORSE[code]
+  MORSE[code]
 end
 
 puts(decode_char('-...'))
 
 def decode_word(code)
-  morse_characters = code.split(' ')
+  morse_characters = code.split
   decoded_characters = []
   morse_characters.each { |char| decoded_characters.push(decode_char(char)) }
-  return decoded_characters.join('')
+  decoded_characters.join
 end
 
-puts (decode_word('-- -.--'))
+puts(decode_word('-- -.--'))
 
 def decode_msg(code)
-  message = code.split("  ")
-  decoded_msg =[]
-  message.each{|char| decoded_msg.push(decode_word(char))}
-  return decoded_msg.join(' ')
+  message = code.split('  ')
+  decoded_msg = []
+  message.each { |char| decoded_msg.push(decode_word(char)) }
+  decoded_msg.join(' ')
 end
-puts (decode_msg("-- -.--   -. .- -- ."))
+puts(decode_msg('-- -.--   -. .- -- .'))
